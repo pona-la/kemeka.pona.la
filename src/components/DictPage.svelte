d279bd3b3ff9af487144e299ffd150da13d9222b
cubedhuang <99danielh@gmail.com>
2026-01-08 23:18:14 -0500

    implement basic field search with fuse.js

--- src/components/DictPage.svelte
+++ src/components/DictPage.svelte
@@ -1,24 +1,21 @@
----
-import type { CollectionEntry } from "astro:content";
-import { marked } from "marked";
+<script lang="ts">
+  import type { CollectionEntry } from "astro:content";
+  import { marked } from "marked";
 
-interface Props {
-  post: CollectionEntry<"content">;
-}
+  interface Props {
+    post: CollectionEntry<"content">;
+  }
 
-const { post } = Astro.props;
-async function md(text: string) {
-  const link_replacer = (_: any, text: string) =>
-    `[${text}](?q=${text.replaceAll(" ", "_")})`;
-  const linked_text = text.replaceAll(/\[(.+?)\](?!\()/g, link_replacer);
-  const html_string = await marked.parse(linked_text);
-  const unembedded_string = html_string
-    .trim()
-    .replaceAll(/^\<p\>/g, "")
-    .replaceAll(/\<\/p\>$/g, "");
-  return unembedded_string;
-}
----
+  const { post }: Props = $props();
+
+  function md(text: string) {
+    const link_replacer = (_: any, text: string) =>
+      `[${text}](?q=${text.replaceAll(" ", "_")})`;
+    const linked_text = text.replaceAll(/\[(.+?)\](?!\()/g, link_replacer);
+    const html_string = marked.parseInline(linked_text, { async: false });
+    return html_string;
+  }
+</script>
 
 <style>
   ol {
@@ -35,34 +32,38 @@ async function md(text: string) {
     margin-bottom: 10px;
   }
 </style>
-<>
-  {
-    post.data.definitions && (
-      <ol>
-        {post.data.definitions.map((definition) => (
-          <li>
-            <span set:html={md(definition.eng)} />
-            <span> — </span>
-            <b set:html={md(definition.tok)} />
-            {definition.examples && (
-              <ul>
-                {definition.examples.map((example) => (
-                  <li>
-                    <i set:html={md(example.tok)} />
-                    <br />
-                    <span set:html={md(example.eng)} />
-                  </li>
-                ))}
-              </ul>
-            )}
-          </li>
-        ))}
-      </ol>
-    )
-  }
-  {post.data.notes && <p set:html={md(post.data.notes)} />}
-</>
-<!-- Implementation of a design by mute ante, more or less --><!-- <style>
+
+{#if post.data.definitions}
+  <ol>
+    {#each post.data.definitions as definition, i (i)}
+      <li>
+        {@html definition.eng} —
+        <b>{@html definition.tok}</b>
+
+        {#if definition.examples}
+          <ul>
+            {#each definition.examples as example}
+              <li>
+                <i>{@html md(example.tok)}</i>
+                <br />
+                <span>{@html md(example.eng)}</span>
+              </li>
+            {/each}
+          </ul>
+        {/if}
+      </li>
+    {/each}
+  </ol>
+{/if}
+
+{#if post.data.notes}
+  <p>
+    {@html post.data.notes}
+  </p>
+{/if}
+
+<!-- Implementation of a design by mute ante, more or less -->
+<!-- <style>
   .keyword {
     color: #1d6bca;
     font-weight: 800;
---
+++ src/components/DictSearch.svelte
@@ -0,0 +1,59 @@
+<script lang="ts">
+  import type { CollectionEntry } from "astro:content";
+  import { onMount } from "svelte";
+  import Fuse from "fuse.js";
+
+  import DictPage from "./DictPage.svelte";
+
+  interface Props {
+    posts: CollectionEntry<"content">[];
+  }
+
+  const { posts }: Props = $props();
+
+  let search = $state("");
+
+  onMount(() => {
+    let params = new URLSearchParams(window.location.search);
+    search = params.get("q")?.replaceAll("_", " ") ?? "";
+  });
+
+  $effect(() => {
+    if (search) {
+      window.history.replaceState(
+        search,
+        "",
+        "/?q=" + search.replaceAll(" ", "_")
+      );
+    } else {
+      window.history.replaceState(search, "", "/");
+    }
+  });
+
+  const fuse = $derived(
+    new Fuse(posts, {
+      keys: [
+        { name: "data.title", weight: 1 },
+        { name: "data.definitions.tok", weight: 0.4 },
+        { name: "data.definitions.eng", weight: 0.4 },
+        { name: "data.definitions.examples.tok", weight: 0.15 },
+        { name: "data.definitions.examples.eng", weight: 0.15 },
+        { name: "notes", weight: 0.15 },
+      ],
+      ignoreLocation: true,
+      threshold: 0.15,
+    })
+  );
+  const filteredPosts = $derived(
+    !search.trim() ? posts : fuse.search(search).map((result) => result.item)
+  );
+</script>
+
+<input type="search" id="search" bind:value={search} />
+
+{#each filteredPosts as post (post.id)}
+  <section>
+    <h2>{post.data.title}</h2>
+    <DictPage {post} />
+  </section>
+{/each}
